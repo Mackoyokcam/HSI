71949ec99c3b202efde5ff6432e617ec44959580
AlexScheller <alexgesc@gmail.com>
2016-11-10 10:01:55 -0800

    just a few more skeleton pages

--- app/views.py
+++ app/views.py
@@ -6,3 +6,11 @@ from app import app
 @app.route('/search', methods=['GET'])
 def search():
 	return render_template("search.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/account')
+def account():
+	return  render_template("account.html")
