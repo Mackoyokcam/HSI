1f1be8713c05bd7736298f73f3c00760e18121fe
Alex Scheller <alexgesc@gmail.com>
2016-11-17 22:21:21 -0800

    now uses js map instead of embedded, also centers map on new searches

--- app/static/javascript/properties.js
+++ app/static/javascript/properties.js
@@ -5,27 +5,27 @@ function id(element) {
 var geocoder;
 var map;
 
-function initialize() {
+function initMap() {
 	geocoder = new google.maps.Geocoder();
-	var bellingham = new google.maps.LatLng(48.749, -122.479); // downtown bellingham
-	var mapOptions = {
-		zoom: 8,
-		center: bellingham;
-	}
-	map = new google.maps.Map(id("map"), mapOptions);
+	var bellingham = {lat: 48.749, lng: -122.479}; // downtown bellingham
+	map = new google.maps.Map(id('map'), {
+		zoom: 12,
+		center: bellingham
+	});
 }
 
 function searchAddress() {
-	var addressText = id("address").value;
+	var addressText = id("search-box").value;
 	geocoder.geocode( {"address": addressText}, function(results, status) {
 		if (status == "OK") {
-			map.setCenter(results[0].geometry.location);
+			var loc = results[0].geometry.location;
+			map.setCenter(loc);
 		} else {
 			alert("Geocode was not successful for the folowing reason:" + status);
 		}
 	});
 }
 
-// window.onload = function() {
-// 	id("search-button").onclick = searchAddress;
-// }
+window.onload = function() {
+	id("search-button").onclick = searchAddress;
+}
