279b9c33eca783bfb20e9b9bd8f9361ac050827b
Mackoyokcam <mark.hangarter@gmail.com>
2017-01-16 23:04:39 -0800

    Added an account creation form, account creation page and updated the views to reflect that

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,26 @@
-from flask.ext.wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, FloatField, HiddenField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
-class searchForm(Form):
+
+class AccountCreationForm(Form):
+    first_name = StringField('First Name', validators=[DataRequired(), Length(max=25)], render_kw={"placeholder": "First Name"})
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(max=25)], render_kw={"placeholder": "Last Name"})
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=35)], render_kw={"placeholder": "Email Address"})
+    password = PasswordField('New Password', validators=[DataRequired(),
+            EqualTo('confirm', message='Passwords must match')], render_kw={"placeholder": "New Password"})
+    confirm = PasswordField('Repeat Password', validators=[DataRequired()], render_kw={"placeholder": "Confirm Password"})
+    address = StringField('Address', validators=[DataRequired()], render_kw={"placeholder": "Address"})
+    apt = StringField('Apartment', validators=[DataRequired()], render_kw={"placeholder": "Apartment #"})
+    city = StringField('City', validators=[DataRequired()], render_kw={"placeholder": "City"})
+    state = StringField('State', validators=[DataRequired()], render_kw={"placeholder": "State"})
+    zip = StringField('Zip', validators=[DataRequired()], render_kw={"placeholder": "Zip"})
+    rent = FloatField('Rent', validators=[DataRequired()], render_kw={"placeholder": "Rent"})
+    gas = FloatField('Gas', validators=[DataRequired()], render_kw={"placeholder": "Gas Bill"})
+    water = FloatField('Water', validators=[DataRequired()], render_kw={"placeholder": "Water Bill"})
+    heating = FloatField('Heating', validators=[DataRequired()], render_kw={"placeholder": "Heating Bill"})
+    electrical = FloatField('Electrical', validators=[DataRequired()], render_kw={"placeholder": "Electrical Bill"})
+    recycle = BooleanField('Recycle?')
+    compost = BooleanField('Compost?')
+
+
--- app/views.py
+++ app/views.py
@@ -1,5 +1,18 @@
-from flask import render_template, request
+from flask import render_template, request, flash, url_for, redirect
 from app import app
+from .forms import AccountCreationForm
+from flask_wtf.csrf import CsrfProtect
+
+
+CsrfProtect(app)
+
+
+''''
+@csrf.error_handler
+def csrf_error(reason):
+	return render_template('csrf_error.html', reason=reason), 400
+'''
+
 
 @app.route('/')
 @app.route('/index')
@@ -7,15 +20,30 @@ from app import app
 def search():
 	return render_template("search.html")
 
+
 @app.route('/about')
 def about():
 	return render_template("about.html")
 
-@app.route('/account')
+
+@app.route('/account', methods=['GET', 'POST'])
 def account():
-	return  render_template("account.html")
+	form = AccountCreationForm()
+	if form.validate_on_submit():
+		# Add User to DB.
+		# Add Info to UtilDB
+		flash("Thanks for registering")
+		return redirect(url_for('search'))
+	return render_template("account_creation.html", form=form)
+
 
 @app.route('/properties', methods=['GET'])
 def properties():
 	search_string = request.args.get('search_string')
 	return render_template("properties.html", search_string=search_string)
+
+
+@app.route('/test', methods=['POST'])
+def test():
+	account_data = request.args.get('account_data')
+	return render_template("test.html", account_data=account_data)
