7c54c7c534de10e5832db9545af19bbb5b51f3bf
Jesse Henson <hensonj2@hsidb.proj.cs.wwu.edu>
2017-02-14 06:52:31 +0000

    JJH: Updated utilQueryTests.py

--- hsi_api/app/utilQueryTests.py
+++ hsi_api/app/utilQueryTests.py
@@ -3,17 +3,15 @@ import hsi_api
 import json
 import Util
 
-class utilQueryTests(origins):
+class utilQueryTests(unittest.TestCase):
     api = None
     def setUp(self):
         self.api = hsi_api.Hsi_Api("config.json")
 
     def test_queryHappy(self):
-        result = self.api.utilQuery([{'long':-122.502782, 'lat':48.729826}])
+        result = self.api.utilQuery([{'long':-122.4968977, 'lat':48.7606482, 'address':"1020 24th st apt c302, Bellingha Wa 98225"}])
         result = json.loads(result)
-        r = ('long' in result['addr0']['""']) \
-            and ('long' in result['addr0']['"M2"') \
-            and (Util.valid_json(str(result)))
+        r = ('long' in result['addr0']['c302']) \
         assertEqual(True, r)
 
 if __name__ == '__main__':
