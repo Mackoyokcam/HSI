b5c99a92f893e0712d9ca281f98cd082c0774268
Jason Ornburn <ornburj@hsidb.proj.cs.wwu.edu>
2017-03-04 07:24:41 +0000

    another test

--- hsi_api/app/views.py
+++ hsi_api/app/views.py
@@ -4,7 +4,6 @@ import json
 import usaddress as usad
 from flask import request
 from werkzeug.datastructures import MultiDict
-import views2
 
 
 CONFIG_FILE_URL = "app/config.json"
@@ -218,3 +217,23 @@ Just a passthrough at this point
 
 def valid(api_key):
     return True
+
+
+@app.route('userDB/addUser', methods = ['POST'])
+def addUser():
+    #param_keys = MultiDict.to_dict(request.form)                                                                                                                                             
+    #api = hsi_api.                                                                                                                                                                           
+    Email = request.form['Email']
+    Password = request.form['Password']
+    Street = request.form['Street']
+    City = request.form['City']
+    State = request.form['State']
+    Zip = request.form['Zip']
+    Apt = request.form['Apt']
+    res = hsi_api_user.addUser(Email,Password, Street, City, State, Zip, Apt)
+    if (res == -1):
+        return "user already in database"
+    elif (res == -2):
+        return "address already in database"
+    else
+        return "user added to database"
