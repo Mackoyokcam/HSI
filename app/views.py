7e2e4dbf66ac292f26c25aeeeefacc01887c8787
AlexScheller <alexgesc@gmail.com>
2017-03-05 14:26:39 -0800

    found it again

--- app/views.py
+++ app/views.py
@@ -163,14 +163,13 @@ def properties():
 			# 	"long" : testingNearbyData["long"],
 			# 	"key" : ""
 			# }
-			# FIX THIS
 			data = {
 				"lat" : "48.722849",
 				"long" : "-122.502782",
 				"key" : ""
 			}
 			res = requests.post("http://140.160.142.77:5000/utilDB/area", data=data)
-			nearbyData = json.dumps(nearbyData)
+			nearbyData = json.dumps(res.text)
 			print("nearby data:\n" + nearbyData)
 			multiUnit = "False"
 			if len(addressData["units"]) > 1:
